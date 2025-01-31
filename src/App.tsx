7e49352c6b1153a15e81ebc7047dc556c6c25eda
suryaprakash69 <suryamsp2020@gmail.com>
2025-01-31 12:51:28 +0530

    fix the file

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from 'react';
 
-const App = () => {
+const App: React.FC = () => {
   return (
     <div>
       app
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
